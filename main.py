fbd79c00f361c9c64d1bf797ab601614711b37ae
Yulia <yulbakanova@gmail.com>
2025-10-22 18:44:42 +0300

    Fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Serverless! 🚀\n", 200, {'Content-Type': 'text/plain'}
+    return "Hello, Serverless! 🚀", 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
